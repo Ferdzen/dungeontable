4e815ed97969d9629785feeb2d93da19f9e62cfd
Ferdzen <fernandaivp63@gmail.com>
2025-05-24 16:37:45 -0300

    add: populando VOs

--- src/main/java/br/edu/utfpr/dungeontable/model/vo/CampaignVO.java
+++ src/main/java/br/edu/utfpr/dungeontable/model/vo/CampaignVO.java
@@ -1,4 +1,43 @@
 package br.edu.utfpr.dungeontable.model.vo;
 
+import lombok.Data;
+
+@Data
 public class CampaignVO {
+    private Integer id;
+    private String name;
+    private String description;
+    private String systemCampaign;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSystemCampaign() {
+        return systemCampaign;
+    }
+
+    public void setSystemCampaign(String systemCampaign) {
+        this.systemCampaign = systemCampaign;
+    }
 }
