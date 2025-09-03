a2b08897d57b6f3de6ed06953a2b9d18ddfea011
Ferdzen <fernandaivp63@gmail.com>
2025-09-03 16:25:03 -0300

    add: permissão para utilização do swagger

--- src/main/java/br/edu/utfpr/dungeontable/security/SecurityConfig.java
+++ src/main/java/br/edu/utfpr/dungeontable/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/login/**").permitAll()
                         .requestMatchers("/api/register/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
 
